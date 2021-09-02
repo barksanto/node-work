abefd75e15b4de23ae7cc7a42df82844924ccb01
Barkley Santo <barkley.santo@gmail.com>
2021-09-02 16:36:08 -0500

    finished os module - saw some built in methods

---
+++ 8-os-module.js
@@ -0,0 +1,18 @@
+// OS Module
+const os = require('os');
+
+//info about current user
+const user = os.userInfo()
+
+//method returns the system uptime in seconds
+const uptime = os.uptime()
+// console.log(`The System Uptime is: ${uptime/60/60} hrs.`);
+
+const currentOS = {
+  name:os.type(),
+  release:os.release(),
+  totalMem:os.totalmem(),
+  freeMem:os.freemem()
+}
+console.log(currentOS)
+
--- app.js
+++ app.js
@@ -1,11 +1,2 @@
-// OS Module
-const os = require('os');
-
-//info about current user
-const user = os.userInfo()
-
-//method returns the system uptime in seconds
-const uptime = os.uptime()
-
-console.log(`The System Uptime is: ${uptime/60/60} hrs.`);
+const path = require('path');
 
