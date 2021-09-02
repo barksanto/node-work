8d2cdbaa65c576a2c0a364b2734452ec13cc9405
Barkley Santo <barkley.santo@gmail.com>
2021-09-02 16:42:42 -0500

    finish path module

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const path = require('path');
 
 const filePath = path.join('/content', 'subfolder', 'test.txt') 
-
 console.log(filePath)
+
+// returns the last thing in that file path
+const base = path.basename(filePath);
+console.log(base)
+
+// resolve- returns an absolute path
+const absolute = path.resolve(__dirname, 'content', 'subfolder', 'test.txt');
+console.log(absolute)
