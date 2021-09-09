f93f56a7186a27deb62424edac4846939c8c5682
Barkley Santo <barkley.santo@gmail.com>
2021-09-09 16:20:19 -0500

    add lodash library, create gitignore

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
+//  this is a dependency we NEED TO INSTALL - 
+//others like http and fs are already a part of Node.
+const _ = require('lodash');
 
+const items = [1,[2,[3, [4]]]];
+const newItems = _.flattenDeep(items)
+console.log(newItems);
